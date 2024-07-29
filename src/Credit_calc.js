c706f7680d6cbff3dd168936c1faea1c28350b96
Yiğit Ata <yucelyigitata@gmail.com>
2024-07-29 16:02:45 +0300

    Table + Upgrades
    Added a new table for your payments and deleted calculator

--- src/Credit_calc.js
+++ src/Credit_calc.js
@@ -1,90 +1,138 @@
 let islem = "ing";
 
-function Kontrol(event) {
-    const number = document.getElementById('loan');
-    const percentage = document.getElementById('installment');
+        function Kontrol(event) {
+            const number = document.getElementById('loan');
+            const percentage = document.getElementById('installment');
 
-    if (number.type !== 'number' || percentage.type !== 'number') {
-        alert('Lütfen girdi türlerini değiştirmeyin!');
-        event.preventDefault();
-        return false;
-    }
+            if (number.type !== 'number' || percentage.type !== 'number') {
+                alert('Lütfen girdi türlerini değiştirmeyin!');
+                event.preventDefault();
+                return false;
+            }
 
-    calculate();
-    event.preventDefault();
-}
+            calculate();
+            event.preventDefault();
+        }
 
-function calculate() {
-    const loan = parseFloat(document.getElementById('loan').value.trim());
-    const installment = parseFloat(document.getElementById('installment').value.trim());
+        function calculate() {
+            const loan = parseFloat(document.getElementById('loan').value.trim());
+            const installment = parseFloat(document.getElementById('installment').value.trim());
 
-    if (isNaN(loan) || isNaN(installment)) {
-        alert('Lütfen boş yer bırakmayınız');
-        return false;
-    }
+            if (isNaN(loan) || isNaN(installment)) {
+                alert('Lütfen boş yer bırakmayınız');
+                return false;
+            }
 
-    let sonuc;
-    switch (islem) {
-        case "ing":
-            sonuc = ING(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        case "hscb":
-            sonuc = HSCB(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        case "icbc":
-            sonuc = ICBC(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        case "aexp":
-            sonuc = AmericanExpress(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        case "capital":
-            sonuc = CapitalOne(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        case "qnb":
-            sonuc = QNB(loan, installment);
-            document.getElementById('result').innerText = "Your loan amount: " + sonuc;
-            break;
-        default:
-            console.log("ERROR!!!");
-    }
-}
+            let sonuc;
+            let monthlyPayment;
+            switch (islem) {
+                case "ing":
+                    sonuc = ING(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                case "hscb":
+                    sonuc = HSCB(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                case "icbc":
+                    sonuc = ICBC(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                case "aexp":
+                    sonuc = AmericanExpress(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                case "capital":
+                    sonuc = CapitalOne(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                case "qnb":
+                    sonuc = QNB(loan, installment);
+                    monthlyPayment = calculateMonthlyPayment(sonuc, installment);
+                    break;
+                default:
+                    console.log("ERROR!!!");
+                    return;
+            }
 
-function handleSelectChange(event) {
-    var selectElement = event.target;
-    islem = selectElement.value;
-}
+            document.getElementById('result').innerText = "Your total loan amount: " + sonuc.toFixed(2);
+            displayTable(monthlyPayment, installment);
+        }
 
-function ING(loan, installment) {
-    var result = loan * (1 + (3.69 / 100) * installment);
-    return result;
-}
+        function handleSelectChange(event) {
+            var selectElement = event.target;
+            islem = selectElement.value;
+        }
 
-function HSCB(loan, installment) {
-    var result = loan * (1 + (7.50 / 100) * installment);
-    return result;
-}
+        function ING(loan, installment) {
+            var result = loan * (1 + (3.69 / 100) * installment);
+            return result;
+        }
 
-function ICBC(loan, installment) {
-    var result = loan * (1 + (4.25 / 100) * installment);
-    return result;
-}
+        function HSCB(loan, installment) {
+            var result = loan * (1 + (7.50 / 100) * installment);
+            return result;
+        }
 
-function AmericanExpress(loan, installment) {
-    var result = loan * (1 + (4.25 / 100) * installment);
-    return result;
-}
+        function ICBC(loan, installment) {
+            var result = loan * (1 + (4.25 / 100) * installment);
+            return result;
+        }
 
-function CapitalOne(loan, installment) {
-    var result = loan * (1 + (4.48 / 100) * installment);
-    return result;
-}
+        function AmericanExpress(loan, installment) {
+            var result = loan * (1 + (4.25 / 100) * installment);
+            return result;
+        }
 
-function QNB(loan, installment) {
-    var result = loan * (1 + (4.49 / 100) * installment);
-    return result;
-}
+        function CapitalOne(loan, installment) {
+            var result = loan * (1 + (4.48 / 100) * installment);
+            return result;
+        }
+
+        function QNB(loan, installment) {
+            var result = loan * (1 + (4.49 / 100) * installment);
+            return result;
+        }
+
+        function calculateMonthlyPayment(totalLoanAmount, months) {
+            return totalLoanAmount / months;
+        }
+
+        function displayTable(monthlyPayment, months) {
+            let tableContainer = document.getElementById('table-container');
+            let tableHTML = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Remaining</th>
+                        <th>Principal</th>
+                        <th>Interest</th>
+                        <th>Tax Paid</th>
+                        <th>Monthly</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+
+            for (let i = 1; i <= months; i++) {
+                let remainingPrincipal = (months - i) * monthlyPayment; 
+                let principalPaid = monthlyPayment; 
+                let interestPaid = 0.25 * monthlyPayment; 
+                let taxPaid = 0.07 * monthlyPayment; 
+
+                tableHTML += `
+                <tr>
+                    <td>${remainingPrincipal.toFixed(2)} TL</td>
+                    <td>${principalPaid.toFixed(2)} TL</td>
+                    <td>${interestPaid.toFixed(2)} TL</td>
+                    <td>${taxPaid.toFixed(2)} TL</td>
+                    <td>${monthlyPayment.toFixed(2)} TL</td>
+                </tr>`;
+            }
+
+            tableHTML += `
+                </tbody>
+            </table>`;
+            tableContainer.innerHTML = tableHTML;
+        }
+
+        
